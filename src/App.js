e2f33c9e4d44a0bf979d60b15c763fb80f3f6200
donblizy <nbliziotis@icloud.com>
2022-03-07 18:02:36 +0000

    refactor for user on header. light styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserContext } from "./contexts/User";
+import { useState } from "react";
 
 //components
 import { Header } from "./components/Header";
 import { ArticleList } from "./components/ArticleList";
 import { Navigator } from "./components/Navigator.jsx";
+import { Home } from "./components/Home";
+
 function App() {
+  const [loggedInUser] = useState("jessjelly");
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Navigator />
-        <Routes>
-          <Route path="/" element={<h1>Welcome to NC News.</h1>} />
-          <Route path="/articles" element={<ArticleList />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <UserContext.Provider value={{ loggedInUser }}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Navigator />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/articles" element={<ArticleList />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
+import { UserContext } from "../contexts/User";
+import { useContext } from "react";
 export const Header = () => {
+  const { loggedInUser } = useContext(UserContext);
   return (
-   <header className="header">
-     <Link to="/" id="headerH3">
+    <header className="header">
+      <Link to="/" id="header__H3">
         <h3>NC News</h3>
         <p id="headerH3">...for news addicts by news addicts.</p>
       </Link>
-   </header>
+      <p className="header__user"> User: {loggedInUser} </p>
+    </header>
   );
 };
