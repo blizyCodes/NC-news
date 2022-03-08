ffb540abf0bf8f06b8d1c60927a2ca3fa1ba9ae6
donblizy <nbliziotis@icloud.com>
2022-03-08 11:36:54 +0000

    capitalise each topic link's first letter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
 export const Home = () => {
   return (
-    <div>
+    <div className="home">
       <h1>Welcome to NC news</h1>
-      <p>Please select a topic or see all articles if you wish to be enlightened. </p>
+      <p className="home__p">
+        Please select a topic or see all articles if you wish to be enlightened.{" "}
+      </p>
     </div>
   );
 };
--- src/components/TopicLinks.jsx
+++ src/components/TopicLinks.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 
 export const TopicList = ({ topic }) => {
-  return <Link className="navigator__link"to={`topics/${topic}`}>{"  "}{topic}{"  "}</Link>;
+  return (
+    <Link className="navigator__link" to={`topics/${topic}`}>
+      {"  "}
+      {topic.charAt(0).toUpperCase() + topic.slice(1)}
+      {"  "}
+    </Link>
+  );
 };
